83893ccd924e1be9bf656e33f603709efa367b34
Rafa <rafacruiz@gmail.com>
2025-11-30 22:54:06 +0100

    done iteration 6 and 7

---
+++ src/components/Message.jsx
@@ -0,0 +1,10 @@
+
+function Message ({ msg }) {
+    return (
+        <p className="message">
+          {msg}
+        </p>
+    );
+}
+
+export default Message;
--- src/components/Tweet.jsx
+++ src/components/Tweet.jsx
@@ -2,25 +2,25 @@
 
 import ProfileImage from "./ProfileImage.jsx";
 import User from "./User.jsx";
+import Timestamp from "./Timestamp.jsx";
+import Message from "./Message.jsx";
 
 
 function Tweet({ tweet }) {
   
   return (
     <div className="tweet">
-      <ProfileImage url={tweet.user.image} />
+      <ProfileImage url={ tweet.user.image } />
     
       <div className="body">
         <div className="top">
           <User name={ tweet.user.name } handle={ tweet.user.handle }/>
 
-          <span className="timestamp">{ tweet.timestamp }</span>
+          <Timestamp timestamp={ tweet.timestamp }/>
         </div>
 
-        <p className="message">
-          {tweet.message}
-        </p>
-
+        <Message msg={ tweet.message } />
+        
         <div className="actions">
           {/* Font Awesome icons */}
           <i className="far fa-comment" data-testid="comment-icon"></i>
